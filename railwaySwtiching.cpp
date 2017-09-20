9f4fce596dffb27e0504e05f4ee0ebc863e2b33b
Zhangyzh36 <771235191@qq.com>
2017-09-20 20:33:30 +0800

    Add files via upload

---
+++ railwaySwtiching.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int count = 0;
+void railwaySwitching(int num);
+void permutation(int *arr, int start, int size);
+bool isValid(int *arr, int size);
+void print(int *arr, int size);
+
+int main()
+{
+	int numOfCars;
+	cout << "Please enter the number of cars:" << endl; 
+	cin >> numOfCars;
+	
+	cout << "The possible results:" << endl;
+	railwaySwitching(numOfCars);
+	cout << count << " ways" << endl;
+	return 0;
+}
+
+void railwaySwitching(int num)
+{
+	int *arr = new int[num];
+	for (int i = 0; i < num; ++i)
+		arr[i] = i + 1;
+	
+	permutation(arr, 0, num);
+}
+
+void permutation(int *arr, int start, int size)
+{
+	if ( start == size - 1 && isValid(arr, size) )
+	{
+		print(arr, size);
+		count++;
+		return;
+	}
+	
+	for (int i = start; i < size; ++i)
+	{
+		int temp = arr[start];
+		arr[start] = arr[i];
+		arr[i] = temp;
+		
+		permutation(arr, start + 1, size);
+		
+		temp = arr[start];
+		arr[start] = arr[i];
+		arr[i] = temp;
+	}
+}
+
+bool isValid(int *arr, int size)
+{
+	int index = 0;
+	
+	stack<int> s;
+	for (int i = 1; i <= size; ++i)
+	{
+		s.push(i);
+		while ( !s.empty() && s.top() == arr[index] ) {
+			s.pop();
+			index++;
+		} 
+	}
+	
+	return s.empty();
+}
+
+void print(int *arr, int size)
+{
+	for (int i = 0; i < size; ++i )
+		cout << arr[i] << " ";
+	cout << endl;
+}
